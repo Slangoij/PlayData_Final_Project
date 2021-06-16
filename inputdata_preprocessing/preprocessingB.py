03d24b755e886dfe1e2bd9ace438db61a0510063
mein0121 <45719589+mein0121@users.noreply.github.com>
2021-06-16 14:50:42 +0900

    planB

--- inputdata_preprocessing/preprocessingB.py
+++ inputdata_preprocessing/preprocessingB.py
@@ -7,17 +7,17 @@ import datetime
 
 ####################################################################
 # webcam 화면 사이즈 조정 파라미터
-wCam, hCam = 1280, 720
+wCam, hCam = 640, 360
 ####################################################################
 
-detector = htm.handDetector(detectionCon=0.75)
+detector = htm.handDetector(min_detection_confidence=0.75)
 
 cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
 
 # model input값 초기화 이하 Canvas
-Canvas = np.zeros((720, 1280, 3), np.uint8)
+Canvas = np.zeros((hCam, wCam, 3), np.uint8)
 input_arr = []  # frame 저장 변수
 
 ########
@@ -46,20 +46,29 @@ while True:
             if input_arr:
                 prev_x, prev_y = input_arr[0]
                 trans_color = 5  # 색 변화를 위해
-                for i in input_arr:
-                    curr_x, curr_y = i
-                    cv2.line(Canvas, (prev_x, prev_y), (curr_x, curr_y), (255, 0, trans_color), 15)
-                    prev_x, prev_y = curr_x, curr_y
-                    if trans_color < 255:
-                        trans_color += 25
+                for i in range(len(input_arr)):
+                    # curr_x, curr_y = i
+                    # cv2.line(Canvas, (prev_x, prev_y), (curr_x, curr_y), (255, 0, trans_color), 15)
+                    # prev_x, prev_y = curr_x, curr_y
+                    # if trans_color < 255:
+                    #     trans_color += 25
+                    # else:
+                    #     trans_color = 255
+                    if i < 5:
+                        curr_x, curr_y = input_arr[i]
+                        cv2.line(Canvas, (prev_x, prev_y), (curr_x, curr_y), (255, 0, trans_color), 15)
+                        prev_x, prev_y = curr_x, curr_y
                     else:
-                        trans_color = 255
+                        curr_x, curr_y = input_arr[i]
+                        cv2.line(Canvas, (prev_x, prev_y), (curr_x, curr_y), (255, 0, 255), 15)
+                        prev_x, prev_y = curr_x, curr_y
+
 
                 # output값을 보기 위한 png파일 변환
                 t = datetime.datetime.now().strftime("%Y-%M-%d %H-%M-%S")
                 cv2.imwrite(os.path.join(img_path, f'{t}.png'), Canvas)
 
-                Canvas = np.zeros((720, 1280, 3), np.uint8)  # Canvas 초기화
+                Canvas = np.zeros((hCam, wCam, 3), np.uint8)  # Canvas 초기화
                 input_arr.clear()
 
         # 손 인식 되면 input_arr에 넣기
@@ -71,19 +80,27 @@ while True:
                 prev_x, prev_y = input_arr[0]
                 trans_color = 5  # 색 변화를 위해
                 for i in range(1, 30):
-                    curr_x, curr_y = input_arr[i]
-                    cv2.line(Canvas, (prev_x, prev_y), (curr_x, curr_y), (255, 0, trans_color), 15)
-                    prev_x, prev_y = curr_x, curr_y
-                    if trans_color < 255:
-                        trans_color += 25
+                    # curr_x, curr_y = input_arr[i]
+                    # cv2.line(Canvas, (prev_x, prev_y), (curr_x, curr_y), (255, 0, trans_color), 15)
+                    # prev_x, prev_y = curr_x, curr_y
+                    # if trans_color < 255:
+                    #     trans_color += 25
+                    # else:
+                    #     trans_color = 255
+                    if i < 5:
+                        curr_x, curr_y = input_arr[i]
+                        cv2.line(Canvas, (prev_x, prev_y), (curr_x, curr_y), (255, 0, trans_color), 15)
+                        prev_x, prev_y = curr_x, curr_y
                     else:
-                        trans_color = 255
+                        curr_x, curr_y = input_arr[i]
+                        cv2.line(Canvas, (prev_x, prev_y), (curr_x, curr_y), (255, 0, 255), 15)
+                        prev_x, prev_y = curr_x, curr_y
 
                 # output값을 보기 위한 png파일 변환
                 t = datetime.datetime.now().strftime("%Y-%M-%d %H-%M-%S")
                 cv2.imwrite(os.path.join(img_path, f'{t}.png'), Canvas)
                 # file_name_cnt += 1
-                Canvas = np.zeros((720, 1280, 3), np.uint8)  # Canvas 초기화
+                Canvas = np.zeros((hCam, wCam, 3), np.uint8)  # Canvas 초기화
 
                 # 먼저 들어온 데이터 빼기(수정 필요)
                 input_arr = input_arr[10:]
