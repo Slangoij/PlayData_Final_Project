d7e0c21ad3294df3b28c43e482f7d255a1e5f6b9
Woosang <yws1502@naver.com>
2021-06-26 02:15:59 +0900

    update draw module

--- common/draw.py
+++ common/draw.py
@@ -4,19 +4,36 @@ import os
 import cv2
 
 def draw_canvas(img, cnt, draw_arr):
-    # 검지
+    # 이미지 데이터 생성
     prev_index = tuple(draw_arr[0])
     for i in range(1, cnt):
-        # 검지
+        '''
+        이전 좌표와 현재 좌표 저장하여 cv2.line을 이용하여 선 연결
+        '''
         curr_index = tuple(draw_arr[i])
         cv2.line(img, prev_index, curr_index, (255, 255, 255), 7)
         prev_index = curr_index
     return img
 
-def save_file(Canvas, draw_arr, img_path, csv_path=None):
+def save_file(Canvas, draw_arr, pred, img_path, csv_path=None):
+    '''
+    모델이 예측한 라벨로 데이터 저장
+        차후 학습 데이터로 사용
+    '''
+    if pred == 0:
+        label = '01next'
+    elif pred == 1:
+        label = '02previous'
+    elif pred == 2:
+        label = '03S'
+    else:
+        label = '04W'
+
     t = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
-    cv2.imwrite(os.path.join(img_path, f'{t}.png'), Canvas)
+    # 이미지 파일 저장
+    cv2.imwrite(os.path.join(img_path, label, f'{t}.png'), Canvas)
     
+    # csv 파일 저장
     if csv_path:
         df = pd.DataFrame(draw_arr)
-        df.to_csv(os.path.join(csv_path, f'{t}.csv'), header=False, index=False)
+        df.to_csv(os.path.join(csv_path, label ,f'{t}.csv'), header=False, index=False)
