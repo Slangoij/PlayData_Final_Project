cbbcc2cf7b30d4823f722dd346ba105270968d2f
mein0121 <45719589+mein0121@users.noreply.github.com>
2021-06-30 09:24:22 +0900

    del

--- deployment/main.py
+++
@@ -1,9 +0,0 @@
-from Demo import demopy
-from PyQt5.QtWidgets import QApplication
-import sys
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    st = demopy()
-    ex = st.demo()
-    sys.exit(app.exec_())
